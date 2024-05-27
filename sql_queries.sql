ef766744f7929bf23065dfe30554c1d266cae1ee
Rahul Nair <rahulmnair1997@gmail.com>
2024-05-27 09:31:44 -0500

    Code update

--- sql_queries.sql
+++ sql_queries.sql
@@ -4,17 +4,15 @@
 
 -- Query
 with base as (
-    select b.brandCode,
+    select ril.brandCode,
            to_char(r.dateScanned, 'YYYY-MM') as year_month,
            count(r.id) as receipts_scanned_count,
            dense_rank() over(order by to_char(r.dateScanned, 'YYYY-MM') desc, count(r.id) desc) as rnk
-    from brands as b
-    join receipts_items_list as ril 
-      on b.brandCode = ril.brandCode
+    from receipts_items_list as ril 
     join receipts as r 
       on ril.receipt_id = r.id
     where to_char(r.dateScanned, 'YYYY-MM') = (select max(to_char(r.dateScanned, 'YYYY-MM')) from receipts)
-    group by b.brandCode,
+    group by ril.brandCode,
              to_char(r.dateScanned, 'YYYY-MM') as year_month
 )
 
